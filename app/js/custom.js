9a6e9a3dfbbdd8838aaac3a4f535a8f379ec54ff
hridoypartho <hridoyhalderpartho@gmail.com>
2022-01-19 16:55:59 +0600

    category item complete

--- app/js/custom.js
+++ app/js/custom.js
@@ -373,3 +373,22 @@ $('.service-category').on('click', 'li', function() {
   $('.service-category li.active').removeClass('active');
   $(this).addClass('active');
 });
+
+$(".mainnav div").click(function() {
+  $("ul").slideToggle();
+  $("ul ul").css("display", "none");
+  $(".mainnav .on").toggleClass("on");
+});
+$(".hasDD").click(function(e) {
+  $(this)
+    .find("> ul")
+    .slideToggle();
+  $(this)
+    .find("> ul ul")
+    .css("display", "none");
+  $(this)
+    .find("> ul li")
+    .removeClass("on");
+  $(this).toggleClass("on");
+  e.stopPropagation();
+});
