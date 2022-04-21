a118e17177cd53f187cd28c3e65c6dd6e48c37de
Zakir Hossen <web.zakirbd@gmail.com>
2022-04-21 16:24:24 +0600

    fix: color issues on color picker

--- app/plugins/theme-mode-switcher/gr-theme-mode-switcher.js
+++ app/plugins/theme-mode-switcher/gr-theme-mode-switcher.js
@@ -21,18 +21,18 @@ const mode_panel_init= () => {
           <h6 class="title">Primary Color</h6>
         </div>
         <ul class="color-skin list-inline">
-          <li data-color="#f51e46" class="color-item primary-color red"></li>
-          <li data-color="#0068e1" class="color-item primary-color blue"></li>
-          <li data-color="#0095b6" class="color-item primary-color bondi-blue"></li>
-          <li data-color="#6453f7" class="color-item primary-color cornflower active"></li>
-          <li data-color="#723881" class="color-item primary-color violet"></li>
-          <li data-color="#fa9928" class="color-item primary-color yellow"></li>
-          <li data-color="#fd6602" class="color-item primary-color orange"></li>
-          <li data-color="#59b210" class="color-item primary-color green"></li>
-          <li data-color="#ff749f" class="color-item primary-color pink"></li>
-          <li data-color="#2a3447" class="color-item primary-color black"></li>
-          <li data-color="#4b0082" class="color-item primary-color indigo"></li>
-          <li data-color="#f8008c" class="color-item primary-color magenta"></li>
+          <li data-color="#f51e46" class="color-item primary-color"></li>
+          <li data-color="#0068e1" class="color-item primary-color"></li>
+          <li data-color="#0095b6" class="color-item primary-color"></li>
+          <li data-color="#6453f7" class="color-item primary-color"></li>
+          <li data-color="#723881" class="color-item primary-color"></li>
+          <li data-color="#fa9928" class="color-item primary-color"></li>
+          <li data-color="#fd6602" class="color-item primary-color"></li>
+          <li data-color="#59b210" class="color-item primary-color"></li>
+          <li data-color="#ff749f" class="color-item primary-color"></li>
+          <li data-color="#2a3447" class="color-item primary-color"></li>
+          <li data-color="#4b0082" class="color-item primary-color"></li>
+          <li data-color="#f8008c" class="color-item primary-color"></li>
         </ul>
       </div>
       <div class="panel-group">
@@ -40,18 +40,18 @@ const mode_panel_init= () => {
           <h6 class="title">Secondary Color</h6>
         </div>
         <ul class="color-skin list-inline">
-          <li data-color="#f51e46" class="color-item secondary-color red"></li>
-          <li data-color="#0068e1" class="color-item secondary-color blue"></li>
-          <li data-color="#0095b6" class="color-item secondary-color bondi-blue"></li>
-          <li data-color="#6453f7" class="color-item secondary-color cornflower active"></li>
-          <li data-color="#723881" class="color-item secondary-color violet"></li>
-          <li data-color="#fa9928" class="color-item secondary-color yellow"></li>
-          <li data-color="#fd6602" class="color-item secondary-color orange"></li>
-          <li data-color="#59b210" class="color-item secondary-color green"></li>
-          <li data-color="#ff749f" class="color-item secondary-color pink"></li>
-          <li data-color="#2a3447" class="color-item secondary-color black"></li>
-          <li data-color="#4b0082" class="color-item secondary-color indigo"></li>
-          <li data-color="#f8008c" class="color-item secondary-color magenta"></li>
+          <li data-color="#f51e46" class="color-item secondary-color"></li>
+          <li data-color="#0068e1" class="color-item secondary-color"></li>
+          <li data-color="#0095b6" class="color-item secondary-color"></li>
+          <li data-color="#6453f7" class="color-item secondary-color"></li>
+          <li data-color="#723881" class="color-item secondary-color"></li>
+          <li data-color="#fa9928" class="color-item secondary-color"></li>
+          <li data-color="#fd6602" class="color-item secondary-color"></li>
+          <li data-color="#59b210" class="color-item secondary-color"></li>
+          <li data-color="#ff749f" class="color-item secondary-color"></li>
+          <li data-color="#2a3447" class="color-item secondary-color"></li>
+          <li data-color="#4b0082" class="color-item secondary-color"></li>
+          <li data-color="#f8008c" class="color-item secondary-color"></li>
         </ul>
       </div>
       <div class="panel-group">
@@ -60,18 +60,18 @@ const mode_panel_init= () => {
           </h6>
         </div>
         <ul class="color-skin list-inline">
-          <li data-color="#f51e46" class="color-item menu-text red"></li>
-          <li data-color="#0068e1" class="color-item menu-text blue"></li>
-          <li data-color="#0095b6" class="color-item menu-text bondi-blue"></li>
-          <li data-color="#6453f7" class="color-item menu-text cornflower"></li>
-          <li data-color="#723881" class="color-item menu-text violet"></li>
-          <li data-color="#fa9928" class="color-item menu-text yellow"></li>
-          <li data-color="#fd6602" class="color-item menu-text orange"></li>
-          <li data-color="#59b210" class="color-item menu-text green"></li>
-          <li data-color="#ff749f" class="color-item menu-text pink"></li>
-          <li data-color="#2a3447" class="color-item menu-text black"></li>
-          <li data-color="#4b0082" class="color-item menu-text indigo"></li>
-          <li data-color="#f8008c" class="color-item menu-text magenta"></li>
+          <li data-color="#f51e46" class="color-item menu-text"></li>
+          <li data-color="#0068e1" class="color-item menu-text"></li>
+          <li data-color="#0095b6" class="color-item menu-text"></li>
+          <li data-color="#6453f7" class="color-item menu-text"></li>
+          <li data-color="#723881" class="color-item menu-text"></li>
+          <li data-color="#fa9928" class="color-item menu-text"></li>
+          <li data-color="#fd6602" class="color-item menu-text"></li>
+          <li data-color="#59b210" class="color-item menu-text"></li>
+          <li data-color="#ff749f" class="color-item menu-text"></li>
+          <li data-color="#2a3447" class="color-item menu-text"></li>
+          <li data-color="#4b0082" class="color-item menu-text"></li>
+          <li data-color="#f8008c" class="color-item menu-text"></li>
         </ul>
       </div>
       <div class="panel-group">
@@ -80,18 +80,18 @@ const mode_panel_init= () => {
           </h6>
         </div>
         <ul class="color-skin list-inline">
-          <li data-color="#f51e46" class="color-item menu-bg red"></li>
-          <li data-color="#0068e1" class="color-item menu-bg blue"></li>
-          <li data-color="#0095b6" class="color-item menu-bg bondi-blue"></li>
-          <li data-color="#6453f7" class="color-item menu-bg cornflower"></li>
-          <li data-color="#723881" class="color-item menu-bg violet"></li>
-          <li data-color="#fa9928" class="color-item menu-bg yellow"></li>
-          <li data-color="#fd6602" class="color-item menu-bg orange"></li>
-          <li data-color="#59b210" class="color-item menu-bg green"></li>
-          <li data-color="#ff749f" class="color-item menu-bg pink"></li>
-          <li data-color="#2a3447" class="color-item menu-bg black"></li>
-          <li data-color="#4b0082" class="color-item menu-bg indigo"></li>
-          <li data-color="#f8008c" class="color-item menu-bg magenta"></li>
+          <li data-color="#f51e46" class="color-item menu-bg"></li>
+          <li data-color="#0068e1" class="color-item menu-bg"></li>
+          <li data-color="#0095b6" class="color-item menu-bg"></li>
+          <li data-color="#6453f7" class="color-item menu-bg"></li>
+          <li data-color="#723881" class="color-item menu-bg"></li>
+          <li data-color="#fa9928" class="color-item menu-bg"></li>
+          <li data-color="#fd6602" class="color-item menu-bg"></li>
+          <li data-color="#59b210" class="color-item menu-bg"></li>
+          <li data-color="#ff749f" class="color-item menu-bg"></li>
+          <li data-color="#2a3447" class="color-item menu-bg"></li>
+          <li data-color="#4b0082" class="color-item menu-bg"></li>
+          <li data-color="#f8008c" class="color-item menu-bg"></li>
         </ul>
       </div>
       <button class="switcher-minimize-button">
@@ -156,6 +156,8 @@ $(document).ready(function(){
   colorPickers.forEach((color) => {
     const colorSets = document.querySelectorAll(color.selector);
     Array.from(colorSets).forEach((item) => {
+      item.style.backgroundColor = item.dataset.color;
+
       item.addEventListener('click', (e) => {
         removeClassFromSiblings(colorSets);
         let clickedItem = e.target;
